35f396d178a7b99e4f4ceeb732dcbcb444135216
Dorji Gyeltshen <dg.dorjigyeltshen@gmail.com>
2024-12-11 00:31:41 +0600

    Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:nuwa_assignment/app/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.remove();
 
   runApp(const NuwaAssignmentApp());
 }
